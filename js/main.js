061fc940de33b111f255625b88d65e6caa7e0400
adysonalves1 <88905769+adysonalves1@users.noreply.github.com>
2022-06-03 23:44:10 -0300

    Add files via upload
    Primeira Versão

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const button = document.querySelector('.button');
+let input = document.querySelector('.input');
+var numero = Math.floor(Math.random() * 1000);
+const container = document.querySelector('.info');
+container.style.display = 'none';
+const manual = document.querySelector('.manual');
+let buttonReset = document.querySelector('.button-reset');
+buttonReset.style.display = 'none';
+
+button.addEventListener('click', (e) => {
+
+    e.preventDefault();
+
+    const span = document.createElement('span');
+
+    if (input.value == numero && input.value != '') {
+
+        manual.style.display = 'none';
+        container.style.display = 'block';
+        span.classList.add('correct');
+        container.insertAdjacentElement('afterbegin',span).innerHTML = `<strong>Você acertou!!!</strong> o número secreto era: ${input.value} <br/>`;
+        buttonReset.style.display = 'block';
+
+    } else if (input.value < numero && input.value != '') {
+        manual.style.display = 'none';
+        container.style.display = 'block';
+        span.classList.add('incorrect');
+        container.insertAdjacentElement('afterbegin',span).innerHTML = `Dica: o número é maior que ${input.value} <br/>`;
+        input.value = '';
+
+    } else if (input.value > numero && input.value != '') {
+        manual.style.display = 'none';
+        container.style.display = 'block';
+        span.classList.add('incorrect');
+        container.insertAdjacentElement('afterbegin',span).innerHTML = `Dica: o número é menor que ${input.value} <br/>`;
+        input.value = '';
+
+
+    } else{
+
+    }
+
+})
